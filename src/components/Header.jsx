a6989dd421f9eedf384a2ba19e2f93c2ebe4a7bf
Alice Cadillon <acadillon@gmail.com>
2025-05-21 15:20:58 +0200

    new font + crea header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import {} from 'react';
+
+const MyComponent = () => {
+
+    // Render
+    return (
+        <>
+
+            <header class="p-margin grid grid-cols-3 gap-margin w-full">
+                <button class="text-left text-title">
+                    <span>Infos</span>
+                </button>
+                <h1 class="text-title">G-architecture</h1>
+            </header>
+
+        </>
+    );
+};
+
+export default MyComponent;

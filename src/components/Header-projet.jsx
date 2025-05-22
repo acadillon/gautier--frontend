7482130e501d4ff7685eab61c2099af10b1b7798
Alice Cadillon <acadillon@gmail.com>
2025-05-22 09:58:46 +0200

    undefined function on button

--- src/components/Header-projet.jsx
+++ src/components/Header-projet.jsx
@@ -8,7 +8,7 @@ const HeaderProjet = () => {
         <>
             <header>
                 <div className='p-margin flex justify-between md:grid md:grid-cols-3 gap-margin w-full'>
-                    <button onClick={handleClick} className="block text-left text-title order-2 md:order-1">
+                    <button className="block text-left text-title order-2 md:order-1">
                         <span className='md:capitalize'>Project Name</span>
                     </button>
                     <h1 className="text-title order-1 md:order-2">G-architecture</h1>

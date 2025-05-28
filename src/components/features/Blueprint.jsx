6ee673926b13ff38134cb2e9272da3757d7c8caf
Alice Cadillon <acadillon@gmail.com>
2025-05-28 12:11:57 +0200

    enter blueprint load anim + title page

--- src/components/features/Blueprint.jsx
+++ src/components/features/Blueprint.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Blueprint = ({ data, goToSlide, activeSlide }) => {
+    const [showBlueprint, setShowBlueprint] = useState(false);
+
     const parseCoordinates = (caption) => {
         if (!caption) return { x: 0, y: 0 };
         
@@ -15,12 +17,17 @@ const Blueprint = ({ data, goToSlide, activeSlide }) => {
         }
 
     };
+
+    const handleBlueprintLoad = () => {
+        setShowBlueprint(true);
+    };
+
     return (
         <>
-            <div className="relative aspect-square w-full px-[30px]">
+            <div className={`relative aspect-square w-full px-[30px] transition-opacity duration-700 delay-100 ease-in-out ${showBlueprint ? "opacity-100" : "opacity-0"}`}>
 
 
-                <img src={data.blueprint.url} alt='blueprint' key={data.blueprint.id} className="aspect-square object-contain w-full h-full" />
+                <img src={data.blueprint.url} alt='blueprint' key={data.blueprint.id} onLoad={handleBlueprintLoad} className="aspect-square object-contain w-full h-full" />
 
                 {data.media.map((media, id) => {
                     const coords = parseCoordinates(media.caption);
